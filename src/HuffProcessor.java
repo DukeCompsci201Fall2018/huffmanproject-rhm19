340acba785bdcc58e14b09c0c207319a05207962
Owen AstrachanHamilton <rhm19@duke.edu>
2018-12-06 23:40:57 -0500

    Test5

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -55,8 +55,8 @@ public class HuffProcessor {
 	}
 		
 	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
-		int bit = in.readBits(BITS_PER_WORD);
 		for (int i = 0; i < codings.length; i++) {
+			int bit = in.readBits(BITS_PER_WORD);
 			if (codings[bit] != null) {
 				String code = codings[bit];
 				out.writeBits(code.length(), Integer.parseInt(code,2));
